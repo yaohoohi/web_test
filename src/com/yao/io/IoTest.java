c3eecaa207ce06673c6920ab1bb03ba5e213dee9
yaofuhai <Administrator@XINYU-20161012I>
2016-11-18 13:40:17 +0800

    sfdsvc

--- src/com/yao/io/IoTest.java
+++ src/com/yao/io/IoTest.java
@@ -10,8 +10,8 @@ import java.io.OutputStreamWriter;
 
 public class IoTest {
 	public static void main(String[] args) {
-		//test1();
-		test2();
+		test1();
+		test2();//mofpdkv[fk[apflk[apslfasfsa
 	}
 
 	private static void test3() {
